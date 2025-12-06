09a9c6344c9e1454ae920a0591ffae2b73bbe62c
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-12-06 21:29:53 +0700

    Update general_dataset.py

--- general_dataset.py
+++ general_dataset.py
@@ -11,6 +11,7 @@ import os
 import os.path as P
 import numpy as np
 import imageio.v2 as imageio
+import torch
 import torch.utils.data as udata
 
 __all__ = ['Dataset_SEGCLS_png', 'MMDataset_memmap', ]
@@ -22,58 +23,78 @@ class General_Dataset_SEGCLS(udata.Dataset):
     INPUT:
         ROOT       -- Root directory of 'split' and 'datapath'
         SPLIT      -- Relative path of split file
-                      e.g. 'split/train.txt'
         DATAPATH   -- Relative path of data folder
-                      e.g. 'data/inbreast'
         MODALITIES -- Tuple of modality names, label must be the first one
-                      e.g. ('label', 'img')
         TRANSFORM  -- Transform imposed on input data (default = None)
-                      e.g. transform=my.transforms.Compose([transforms.ToTensor(),
-                              transforms.Normalize(),])
-        SHAPE      -- (For reading memmap data) Shape of memmap data
-                      e.g. (512, 384)
 
-    OUTPUT ( RETURNED BY __GETITEM__ ) :
-        (IMG, LABEL) -- (C, H, W) as np.float32 and (H, W) as np.uint8 for ''img'' and ''label'' respectively
+    OUTPUT:
+        IMG  -- Tensor (C, H, W), float32
+        LABEL -- Tensor (H, W), long, binary (0/1)
+        CLS_LABEL -- 0 or 1 (image-level label)
     """
     def __init__(self, root, split, datapath, modalities, transform=None, shape=None):
         self.transform = transform
         self.data = []
         self.label = []
         
-        # Load subject names into a list
+        # load list of sample names
         split_fname = os.path.join(root, split)
         sn_list = open(split_fname, 'r').read().splitlines()
         
         for sn in sn_list:
+            # read mask
             label_ = self.access_data(root, datapath, modalities[0], sn, 'uint8', shape)
             self.label.append(label_)
             
+            # read image modalities
             img_ = []
             for mod in modalities[1:]:
                 image = self.access_data(root, datapath, mod, sn, 'float32', shape)
                 img_.append(image)
-            img_ = np.stack(img_)
+            img_ = np.stack(img_)  # (C, H, W)
             self.data.append(img_)
             
+
     def access_data(self, root, datapath, mod, sn, dtype, shape=None):
+        # implemented in subclass
         pass
     
+
     def __getitem__(self, index):
         img, label = self.data[index], self.label[index]
 
+        # ----------------------------------------------------------
+        # 1) BINARY HOÁ LABEL: pixel > 0 → 1, pixel == 0 → 0
+        # ----------------------------------------------------------
+        label = (label > 0).astype(np.uint8)
+
+        # ----------------------------------------------------------
+        # 2) Chuyển sang Tensor LONG (bắt buộc cho CrossEntropyLoss)
+        # ----------------------------------------------------------
+        label = torch.from_numpy(label).long()
+
+        # ----------------------------------------------------------
+        # 3) Transform (nếu transform trả Tensor → không ảnh hưởng)
+        # ----------------------------------------------------------
         if self.transform is not None:
             img, label = self.transform(img, label)
-            
-        if label.sum() > 0:
-            cls_label = 1
+
         else:
-            cls_label = 0
+            # nếu không có transform, convert ảnh sang Tensor luôn
+            img = torch.from_numpy(img).float()
+
+        # ----------------------------------------------------------
+        # 4) Image-level label (classification target)
+        # ----------------------------------------------------------
+        cls_label = 1 if label.sum() > 0 else 0
+        cls_label = torch.tensor(cls_label).long()
 
         return img, label, cls_label
-        
+    
+
     def __len__(self):
         return len(self.data)
+
 
 
 class Dataset_SEGCLS_png(General_Dataset_SEGCLS):
@@ -82,35 +103,20 @@ class Dataset_SEGCLS_png(General_Dataset_SEGCLS):
         data = imageio.imread(fname)
         return data.astype(dtype)
 
+
+
 class MMDataset_memmap(udata.Dataset):
     """
-    Multi-Modality Dataset
-    INPUT:
-        ROOT       -- Root directory of 'split' and 'datapath'
-        SPLIT      -- Relative path of split file
-                      e.g. 'split/train.txt'
-        DATAPATH   -- Relative path of data folder
-                      e.g. 'data/'
-        MODALITIES -- Tuple of modality names, label must be the first one
-                      e.g. ('label', 'img')
-        TRANSFORM  -- Transform imposed on input data (default = None)
-                      e.g. transform=my.transforms.Compose([transforms.ToTensor(),
-                              transforms.Normalize(),])
-
-    OUTPUT ( RETURNED BY __GETITEM__ ) :
-        (IMG, LABEL) -- (C, H, W) as np.float32 and (H, W) as np.uint8 for ''img'' and ''label'' respectively
+    Multi-Modality Dataset (binary mask + image .dat format)
     """
-
     def __init__(self, root, split, datapath, modalities, transform=None, shape=None):
         self.transform = transform
         self.data = []
         self.label = []
 
-        # Load subject names into a list
         split_fname = os.path.join(root, split)
         sn_list = open(split_fname, 'r').read().splitlines()
 
-        # Load images and labels
         for sn in sn_list:
             label_fn = os.path.join(root, datapath, modalities[0], '%s.dat' % sn)
             label_ = np.memmap(label_fn, dtype='uint8', mode='r', shape=shape[1:])
@@ -121,26 +127,19 @@ class MMDataset_memmap(udata.Dataset):
             self.data.append(img_)
             self.label.append(label_)
 
-#        self.data = np.stack(self.data) # (N, C, D, H, W)
-#        self.label = np.stack(self.label) # (N, D, H, W)
-
-#        del img_, label_
-
     def __getitem__(self, index):
         img, label = self.data[index], self.label[index]
 
+        # Binarize mask
+        label = (label > 0).astype(np.uint8)
+        label = torch.from_numpy(label).long()
+
         if self.transform is not None:
             img, label = self.transform(img, label)
+        else:
+            img = torch.from_numpy(img).float()
 
         return img, label
 
     def __len__(self):
         return len(self.data)
-        
-        
-        
-        
-        
-        
-        
-        
